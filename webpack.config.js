3620d0721769857201b8c4693675de923ddf10da
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2019-10-22 17:48:03 +0200

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
-    watch: true,
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
